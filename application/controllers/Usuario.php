e24bb2b742ec11a259c4bc8b5fbc5f63537c085b
Vitor Mantovani <vtrmantovani@gmail.com>
2016-03-07 09:46:55 -0300

    tela de cadastro de usuario

--- application/controllers/Usuario.php
+++ application/controllers/Usuario.php
@@ -1,12 +1,15 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
 
 class Usuario extends CI_Controller {
 
-	public function __construct()
-	{
-		parent::__construct();
+    public function __construct() {
+        parent::__construct();
 
-		$this->load->model('usuario_model', 'usuario_model');
+        $this->load->model('usuario_model', 'usuario_model');
+        $this->load->model('perfil_model', 'perfil_model');
 
         if (isset($_SESSION['userLogin'])) {
             if (strtoupper($_SESSION['userLogin']['tipoAcesso']) == 'USUARIO') {
@@ -14,14 +17,13 @@ class Usuario extends CI_Controller {
             }
         } else {
             redirect('/', 'refresh');
-        }	
-	}
+        }
+    }
 
-	public function index()
-	{
-		$data = ['base_url' => $this->config->base_url()];
-        $this->twig->display('usuario/dashboard_usuario', $data);		
-	}
+    public function index() {
+        $data = ['base_url' => $this->config->base_url()];
+        $this->twig->display('usuario/dashboard_usuario', $data);
+    }
 
     public function listar() {
         $message_success = $this->session->flashdata('message_success');
@@ -30,5 +32,14 @@ class Usuario extends CI_Controller {
             'usuarios' => $this->usuario_model->get_usuario_notDeleted()->result()
         ];
         $this->twig->display('usuario/listar', $data);
-    }	
+    }
+
+    // Formulario que adiciona novo usuario
+    public function novo() {
+        $data = ['base_url' => $this->config->base_url(),
+            'tipo_perfils' => $this->perfil_model->get_tiposPerfil_all()->result()
+        ];
+        $this->twig->display('usuario/novo', $data);
+    }
+
 }
