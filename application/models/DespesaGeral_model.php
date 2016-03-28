1f1b1075e81da32f96e577d897a7e4f6393972da
Vitor Mantovani <vtrmantovani@gmail.com>
2016-03-28 10:21:29 -0300

    mostrar nome do status e da categoria na listagem

--- application/models/DespesaGeral_model.php
+++ application/models/DespesaGeral_model.php
@@ -4,32 +4,36 @@ if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 
 class DespesaGeral_model extends CI_Model {
-    
+
     public function get_despesageral_all() {
         return $this->db->get('despesa');
     }
-    
+
     public function get_despesageral_notDeleted() {
-        $this->db->where('deletado', 0);
-        return $this->db->get('despesa');
+        $this->db->select('despesa.id_despesa,despesa.data_criacao,despesa.data_vencimento,despesa.data_pagamento,despesa.total,despesa_status.titulo as status, despesa_categoria.titulo as categoria');
+        $this->db->from('despesa');
+        $this->db->join('despesa_status', 'despesa_status.id_status = despesa.id_despesa');
+        $this->db->join('despesa_categoria', 'despesa_categoria.id_categoria = despesa.id_despesa');
+        $this->db->where('despesa.deletado', 0);
+        return $this->db->get();
     }
-    
-     public function get_despesageral_byid($id_despesa) {
+
+    public function get_despesageral_byid($id_despesa) {
         $this->db->where('id_despesa', $id_despesa);
         $this->db->limit(1);
         return $this->db->get('despesa');
     }
-    
-    public function get_despesageral_count_notDeleted(){
+
+    public function get_despesageral_count_notDeleted() {
         $this->db->where('deletado', 0);
         $db_result = $this->db->get('despesa');
         return $db_result->num_rows();
     }
-    
+
     public function insert_despesageral($despesa) {
         $this->db->insert('despesa', $despesa);
     }
-    
+
     public function update_despesageral($despesa, $id_despesa) {
         $this->db->update('despesa', $despesa, array('id_despesa' => $id_despesa));
     }
@@ -43,5 +47,5 @@ class DespesaGeral_model extends CI_Model {
             $this->db->update('despesa', array('deletado' => 1), $condicao);
         }
     }
-    
+
 }
