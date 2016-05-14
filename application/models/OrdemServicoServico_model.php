e822f3ecbf4e15bf5cc6e13736b754ab0460eca0
Eduardo Araujo <eduardo.araujo0@outlook.com>
2016-05-14 00:39:19 -0300

    Criado model para ordem de serviço serviços

---
+++ application/models/OrdemServicoServico_model.php
@@ -0,0 +1,31 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class OrdemServicoServico_model extends CI_Model {
+
+	public function insert_orcamentoServicoServico($ordemServicoServico) {
+        $this->db->insert('ordem_servico_servico', $orcamento);     
+    }
+
+    public function get_orcamentoServicoServico_byid($id = NULL) {
+        if ($id <> NULL) {
+            $this->db->where('id_ordem_servico', $id);
+
+            return $this->db->get('ordem_servico_servico');
+        } else {
+            return NULL;
+        }
+    }
+
+    public function delete_orcamentoServicoServico($condicao = NULL) {
+        if ($condicao <> NULL) {
+            $this->db->delete("ordem_servico_servico", $condicao);
+        }
+    }    
+
+    public function delete_logical_orcamentoServicoServico($condicao = NULL) {
+        if ($condicao <> NULL) {
+            $this->db->update('ordem_servico_servico', array('deletado' => 1), $condicao);
+        }
+    }  
+}
