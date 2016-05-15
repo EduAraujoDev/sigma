e14d4428beed6c60c81e470eb34dc11fbc52c0b7
Eduardo Araujo <eduardo.araujo0@outlook.com>
2016-05-15 19:03:38 -0300

    Implementado a visualização da ordem de serviço

--- application/models/OrdemServicoServico_model.php
+++ application/models/OrdemServicoServico_model.php
@@ -3,11 +3,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class OrdemServicoServico_model extends CI_Model {
 
-	public function insert_orcamentoServicoServico($ordemServicoServico) {
+	public function insert_ordemServicoServico($ordemServicoServico) {
         $this->db->insert('ordem_servico_servico', $ordemServicoServico);     
     }
 
-    public function get_orcamentoServicoServico_byid($id = NULL) {
+    public function get_ordemServicoServico_byid($id = NULL) {
         if ($id <> NULL) {
             $this->db->where('id_ordem_servico', $id);
 
@@ -17,13 +17,13 @@ class OrdemServicoServico_model extends CI_Model {
         }
     }
 
-    public function delete_orcamentoServicoServico($condicao = NULL) {
+    public function delete_ordemServicoServico($condicao = NULL) {
         if ($condicao <> NULL) {
             $this->db->delete("ordem_servico_servico", $condicao);
         }
     }    
 
-    public function delete_logical_orcamentoServicoServico($condicao = NULL) {
+    public function delete_logical_ordemServicoServico($condicao = NULL) {
         if ($condicao <> NULL) {
             $this->db->update('ordem_servico_servico', array('deletado' => 1), $condicao);
         }
